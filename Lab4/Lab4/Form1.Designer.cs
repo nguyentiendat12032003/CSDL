cd83bd88e4f671e64a92306cf55484c398463a04
vuminhquan596704 <vuminhquan596704@gmail.com>
2024-05-10 15:04:04 +0700

    add

--- Lab4/Lab4/Form1.Designer.cs
+++ Lab4/Lab4/Form1.Designer.cs
@@ -577,7 +577,7 @@ namespace Lab4
             this.label11.TabIndex = 28;
             this.label11.Text = "Employee_ID:";
             // 
-            // label12
+            // label12q
             // 
             this.label12.AutoSize = true;
             this.label12.Location = new System.Drawing.Point(17, 116);
